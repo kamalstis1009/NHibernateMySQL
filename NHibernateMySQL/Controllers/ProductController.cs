c376fcb27ad3255b1b3af931c1ed63ffcaa6b9d1
Mustofa Kamal <himustofa@outlook.com>
2020-12-26 12:24:51 +0600

    Initial commit

--- NHibernateMySQL/Controllers/ProductController.cs
+++ NHibernateMySQL/Controllers/ProductController.cs
@@ -31,5 +31,85 @@ namespace NHibernateMySQL.Controllers
                 return NotFound();
             }
         }
+
+        //[Authorize]
+        [Route("Api/Task/GetProductById", Name = "GetProductById")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetProductById([FromUri] int id)
+        {
+            using (ISession session = NHibertnateSession.OpenSession())
+            {
+                var response = await session.Query<Products>().Where(index => index.Id == id).FirstOrDefaultAsync();
+                if (response != null)
+                {
+                    return Ok(response);
+                }
+                return NotFound();
+            }
+        }
+
+        //======================================================| Add
+        [Route("Api/Product/Add")]
+        [HttpPost]
+        public async Task<IHttpActionResult> AddTask([FromBody] Products item)
+        {
+            using (ISession session = NHibertnateSession.OpenSession())
+            {
+                var response = await session.SaveAsync(item);
+                if (response != null)
+                {
+                    //return Ok(response);
+                    return CreatedAtRoute(nameof(GetProductById), new { id = item.Id }, item); //https://www.jasoncavett.com/blog/converting-to-attribute-routing-in-webapi-applications/
+                }
+                return NotFound();
+            }
+        }
+
+        //======================================================| Delete
+        //[Authorize]
+        [Route("Api/Product/Delete")]
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteTaskById([FromUri] int id)
+        {
+            using (ISession session = NHibertnateSession.OpenSession())
+            {
+                var model = await session.Query<Products>().Where(index => index.Id == id).FirstOrDefaultAsync();
+                if (model != null)
+                {
+                    var response = session.DeleteAsync(model);
+                    if (response != null)
+                    {
+                        return Ok(response);
+                    }
+                    return NotFound();
+                }
+                return NotFound();
+            }
+        }
+
+        //======================================================| Put/Update
+        [Route("Api/Product/Update")]
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateTaskById([FromUri] int id, [FromBody] Products item)
+        {
+            using (ISession session = NHibertnateSession.OpenSession())
+            {
+                var model = await session.Query<Products>().Where(index => index.Id == id).FirstOrDefaultAsync();
+                if (model != null)
+                {
+                    model.Name = item.Name;
+                    model.Brand = item.Brand;
+                }
+                var response = session.SaveOrUpdateAsync(model);
+                if (response != null)
+                {
+                    return Ok(response);
+                }
+                return NotFound();
+            }
+        }
+
+
+
     }
 }
